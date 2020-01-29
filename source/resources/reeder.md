e7b103bf69d220c9b2b633b3870b1a0d48eb2920
FiTeen <isfiteen@icloud.com>
2020-01-30 02:42:49 +0800

    feat: add valine.js

--- source/resources/reeder.md
+++
@@ -1,7 +0,0 @@
----
-title: Reeder 4 Mac 版破解
----
-
-> ⚠️⚠️⚠️ **请支持[正版](http://reederapp.com)，仅供技术交流。**
-
-可以在网上直接搜到破解版。例如[百度网盘](https://pan.baidu.com/s/1D7Ry5zHgSQ6A3UP7CfbYow) 密码：7rg1
--- themes/inside/lib/filter/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (hexo) {
-  hexo.extend.filter.register('after_post_render', require('./post'));
-  hexo.extend.filter.register('template_locals', require('./templates'));
-};
